413b71e6bac7a9e1dd1b2c2c917086736711b563
frost-nzcr4 <frost-nzcr4@gmail>
2011-04-17 01:33:57 +0400

    fix js loading and possibly namespace error when using $

--- web/js/galleryInstance.js
+++ web/js/galleryInstance.js
@@ -1,21 +1,21 @@
-$().ready(function() {
-
-    // We only want these styles applied when javascript is enabled
-    $('div.navigation').css({'width' : '95%', 'float' : 'left'});
-    $('div.content').css('display', 'block');
-
-    // Initially set opacity on thumbs and add
-    // additional styling for hover effect on thumbs
-    var onMouseOutOpacity = 0.67;
-    $('#thumbs ul.thumbs li').opacityrollover({
-            mouseOutOpacity:   onMouseOutOpacity,
-            mouseOverOpacity:  1.0,
-            fadeSpeed:         'fast',
-            exemptionSelector: '.selected'
-    });
-
-    // Initialize Advanced Galleriffic Gallery
-    var gallery = $('#thumbs').galleriffic({
+(function ($) {
+	$(document).ready(function () {
+	    // We only want these styles applied when javascript is enabled
+	    $('div.navigation').css({'width' : '95%', 'float' : 'left'});
+	    $('div.content').css('display', 'block');
+	
+	    // Initially set opacity on thumbs and add
+	    // additional styling for hover effect on thumbs
+	    var onMouseOutOpacity = 0.67;
+	    $('#thumbs ul.thumbs li').opacityrollover({
+	        mouseOutOpacity:   onMouseOutOpacity,
+	        mouseOverOpacity:  1.0,
+	        fadeSpeed:         'fast',
+	        exemptionSelector: '.selected'
+	    });
+	
+	    // Initialize Advanced Galleriffic Gallery
+	    var gallery = $('#thumbs').galleriffic({
             delay:                     2500,
             numThumbs:                 15,
             preloadAhead:              10,
@@ -39,16 +39,17 @@ $().ready(function() {
             syncTransitions:           true,
             defaultTransitionDuration: 0,
             onSlideChange:             function(prevIndex, nextIndex) {
-                    // 'this' refers to the gallery, which is an extension of $('#thumbs')
-                    this.find('ul.thumbs').children()
-                            .eq(prevIndex).fadeTo('fast', onMouseOutOpacity).end()
-                            .eq(nextIndex).fadeTo('fast', 1.0);
+                // 'this' refers to the gallery, which is an extension of $('#thumbs')
+                this.find('ul.thumbs').children()
+                    .eq(prevIndex).fadeTo('fast', onMouseOutOpacity).end()
+                    .eq(nextIndex).fadeTo('fast', 1.0);
             },
             onPageTransitionOut:       function(callback) {
-                    this.fadeTo('fast', 0.0, callback);
+                this.fadeTo('fast', 0.0, callback);
             },
             onPageTransitionIn:        function() {
-                    this.fadeTo('fast', 1.0);
+                this.fadeTo('fast', 1.0);
             }
-    });
-});
+	    });
+	});
+})(jQuery);
